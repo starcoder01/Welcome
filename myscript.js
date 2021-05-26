16141f9824413415ed5faaee3648fa735ef37f94
Sneha Banga <79856550+starcoder01@users.noreply.github.com>
2021-05-26 18:27:37 +0530

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,23 @@
+function myfun()
+{
+    var a=document.getElementById('myList');
+    if(a.style.display !== "block"){
+        a.style.display = "block";
+    }
+
+    else{
+        a.style.display="none";
+    }
+}
+
+function myfun2()
+{
+    var a=document.getElementById('tech');
+    if(a.style.display !== "block"){
+        a.style.display = "block";
+    }
+
+    else{
+        a.style.display="none";
+    }
+}
